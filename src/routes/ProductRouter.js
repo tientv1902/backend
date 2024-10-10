15d77ece1bd834e3e2fe1e675f490c7f25f25e61
Tong Viet Tien <tongviettien1902@gmail.com>
2024-10-10 16:48:37 +0700

    CRUD product

---
+++ src/routes/ProductRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router()
+const ProductController = require('../controllers/ProductController')
+const { authMiddleware } = require("../middleware/authMiddleware");
+
+router.post('/create', ProductController.createProduct)
+router.put('/update/:id', authMiddleware ,ProductController.updateProduct)
+router.get('/get-details/:id', ProductController.getDetailsProduct)
+router.delete('/delete/:id', ProductController.deleteProduct)
+router.get('/getAll', ProductController.getAllProduct)
+
+module.exports = router  
