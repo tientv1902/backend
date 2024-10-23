3a1ab8d9b0c569cf487da581b77599b50e3aab00
Tong Viet Tien <tongviettien1902@gmail.com>
2024-10-23 21:08:46 +0700

    update code login log out , auth and create profile

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const { default: mongoose } = require("mongoose");
 const routes = require('./routes');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 dotenv.config()
@@ -12,7 +12,9 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.use(bodyParser.json())
 app.use(cookieParser())
 
 routes(app);
